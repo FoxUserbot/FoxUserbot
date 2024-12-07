33045c1e2d55d1b78beaf85c472870d5fed93b53
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2024-12-07 23:50:36 +0300

    Похуй, ГООООООООООООЛ

--- configurator.py
+++ configurator.py
@@ -1,23 +1,17 @@
+ 
 import os
 import sys
 import configparser
-
-
-config_id = "2860432"
-config_hash = "2fde6ca0f8ae7bb58844457a239c7214"
+config_id = "29679445"
+config_hash = "e656a7489649d542ceb3c326f54345ba"
 config_model = "FoxUserbot"
-
 config = configparser.ConfigParser()
 config.read("config.ini")
-
-
 def api():
     get_id = config.get("pyrogram", "api_id")
     get_hash = config.get("pyrogram", "api_hash")
     get_device_model = config.get("pyrogram", "device_model")
     return get_id, get_hash, get_device_model
-
-
 def my_api():
     try:
         api_id, api_hash, device_model = api()
@@ -28,7 +22,6 @@ def my_api():
         config.set("pyrogram", "device_model", config_model)
         with open("config.ini", "w") as config_file:
             config.write(config_file)
-
         api_id = config_id
         api_hash = config_hash
         device_model = config_model
