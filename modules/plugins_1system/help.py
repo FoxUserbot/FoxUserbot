3876cac7bb14273e218a599e0a57ac28d3e1a588
ArThirtyFour <149522975+ArThirtyFour@users.noreply.github.com>
2025-07-05 21:49:59 +0300

    2.2 Update [Complete]

--- modules/plugins_1system/help.py
+++ modules/plugins_1system/help.py
@@ -26,34 +26,21 @@ def get_text(message):
     if message.from_user.is_premium:
         return f"""
 <emoji id="5190875290439525089">😊</emoji><b> | FoxUserbot RUNNING</b>
-<emoji id="5197288647275071607">🛡</emoji><b> | Version: </b><b>{version}</b><b>
-<emoji id="5372878077250519677">📱</emoji><b> | Python: {python_version()}</b>
-<emoji id="5190637731503415052">🦊</emoji><b> | Kurigram: {__version__}</b>
+<emoji id="5197288647275071607">🛡</emoji><b> | Version: </b><b>{version}</b>
 <emoji id="5193177581888755275">💻</emoji><b> | Modules: {len(module_list)}</b>
-
+<emoji id="5444856076954520455">🧾</emoji><b> | Prefix: {my_prefix()}</b>
 <emoji id="5436113877181941026">❓</emoji><a href="{link}"><b> | List of all commands. </b></a>
-<emoji id="5330237710655306682">📱</emoji><a href="https://t.me/foxteam0"><b> | Official FoxTeam Channel.</b></a>
-<emoji id="5346181118884331907">📱</emoji><a href="https://github.com/FoxUserbot/FoxUserbot"><b> | Github Repository.</b></a>
-<emoji id="5379999674193172777">🔭</emoji><a href="https://github.com/FoxUserbot/FoxUserbot#how-to-install"><b> | Installation Guide.</b></a>
 
-<emoji id="5361756547200351182">😈</emoji> | Thanks for using FoxUserbot.
-<emoji id="5359700554945689675">😘</emoji> | If you find a malfunction, write issues in github.
 """
     else:
         return f"""
 <b>🦊 | FoxUserbot RUNNING</b>
 <b>🔒 | Version: {version}</b>
-<b>🐍 | Python: {python_version()}</b>
-<b>🥧 | Kurigram: {__version__}</b>
 <b>💼 | Modules: {len(module_list)}</b>
-
+<b>🔒 | Prefix: {my_prefix()}</b>
 <b><a href={link}>❓ | List of all commands. </a></b>
-<b><a href="https://t.me/foxteam0">💻 | Official FoxTeam Channel.</a></b>
-<b><a href="https://github.com/FoxUserbot/FoxUserbot">🐈‍⬛ | Github Repository.</a></b>
-<b><a href="https://github.com/FoxUserbot/FoxUserbot#how-to-install">🤔 | Installation Guide.</a></b>
 
-❤️ | Thanks for using FoxUserbot.
-❤️ | If you find a malfunction, write issues in github."""
+"""
 
 
 
