8fd70cb1b9f58a0d9d9afe70a00f00f45c023dd2
A9FM [aka Artur Lamerov] <201252757+a9fmofficial@users.noreply.github.com>
2025-07-18 05:08:15 +0300

    bugfix

--- modules/plugins_1system/1merge_from_old_version.py
+++ modules/plugins_1system/1merge_from_old_version.py
@@ -10,7 +10,7 @@ if os.path.isdir("plugins"):
     os.rename("plugins", f"modules_old_{i}")
     Write.Print(f"""[WARNING] Old incompatible modules (modules_old_{i}) detected!
 [WARNING] Please rewrite them in a new format and upload them to the
-[WARNING] ==> .modules\plugins_2custom directory.\n""", Colors.red_to_yellow, interval=0.0)
+[WARNING] ==> .modules/plugins_2custom directory\n""", Colors.red_to_yellow, interval=0.0)
 
 # 2.1 > 2.2
 if os.path.exists("modules/plugins_1system/support.py"):
