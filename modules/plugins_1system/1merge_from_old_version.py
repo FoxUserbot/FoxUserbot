a6ea1c960d58c82b1f5306726032bf78905c0b5e
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-07-25 06:01:50 +0300

    Fix

--- modules/plugins_1system/1merge_from_old_version.py
+++ modules/plugins_1system/1merge_from_old_version.py
@@ -8,9 +8,9 @@ import wget
 if os.path.isdir("plugins"):
     i = random.randint(10000, 99999)
     os.rename("plugins", f"modules_old_{i}")
-    Write.Print(f"""[WARNING] Old incompatible modules (modules_old_{i}) detected!
+    print(f"""[WARNING] Old incompatible modules (modules_old_{i}) detected!
 [WARNING] Please rewrite them in a new format and upload them to the
-[WARNING] ==> .modules/plugins_2custom directory\n""", Colors.red_to_yellow, interval=0.0)
+[WARNING] ==> .modules/plugins_2custom directory\n""")
 
 # 2.1 > 2.2
 if os.path.exists("modules/plugins_1system/support.py"):
