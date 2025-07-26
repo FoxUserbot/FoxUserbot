9cea24fd524cb62a329aa5c720a32f1b876c3835
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-07-26 03:56:24 +0300

    Move config.ini to userdata/config.ini

--- configurator.py
+++ configurator.py
@@ -1,17 +1,23 @@
- 
 import os
 import sys
 import configparser
-config_id = "29679445"
-config_hash = "e656a7489649d542ceb3c326f54345ba"
+
+# default values
+config_id = "2860432"
+config_hash = "2fde6ca0f8ae7bb58844457a239c7214"
 config_model = "FoxUserbot"
+
+PATH_FILE = "userdata/config.ini"
+
 config = configparser.ConfigParser()
-config.read("config.ini")
+config.read(PATH_FILE)
+
 def api():
     get_id = config.get("pyrogram", "api_id")
     get_hash = config.get("pyrogram", "api_hash")
     get_device_model = config.get("pyrogram", "device_model")
     return get_id, get_hash, get_device_model
+
 def my_api():
     try:
         api_id, api_hash, device_model = api()
@@ -20,8 +26,10 @@ def my_api():
         config.set("pyrogram", "api_id", config_id)
         config.set("pyrogram", "api_hash", config_hash)
         config.set("pyrogram", "device_model", config_model)
-        with open("config.ini", "w") as config_file:
+        
+        with open(PATH_FILE, "w") as config_file:
             config.write(config_file)
+            
         api_id = config_id
         api_hash = config_hash
         device_model = config_model
