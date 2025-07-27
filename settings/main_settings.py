6fadd6dd0baf24cca72d66722256b05b7674f899
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-07-27 05:10:02 +0300

    Hotfix

---
+++ settings/main_settings.py
@@ -0,0 +1,5 @@
+"""Please, ignore this file."""
+
+version = "2.3.5.0"
+module_list = {}
+file_list = {}
---
+++ uptime.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+bot_start_time = datetime.now()
+
+def bot_start_time():
+    return bot_start_time
