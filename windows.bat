80aa9c12e46b083ad89828148082bbb4ea2a815e
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-11-09 22:46:30 +0300

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,27 +1,69 @@
 @echo off
-chcp 65001 > nul
+chcp 65001
 cls
 title FoxUserBot
 
-where python >nul 2>&1
+echo Starting FoxUserBot...
+
+py --version >nul 2>&1
 if %errorlevel% equ 0 (
-    echo Python found on system 
+    echo Using Python via py launcher
+    py main.py
+    pause
+    exit /b
+)
+
+python --version >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Using Python via python command
     python main.py
     pause
     exit /b
 )
 
-echo Python not found...
+echo Searching for Python in common locations...
 
-winget --version >nul 2>&1
-if errorlevel 1 (
-    echo Install Winget...
-    powershell -Command "Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'; Start-Process -FilePath 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -ArgumentList '/quiet' -Wait"
-    echo Winget installed succesfully
+set FOUND=0
+
+for %%p in (
+    C:\Python312\python.exe
+    C:\Python311\python.exe
+    C:\Python310\python.exe
+    C:\Python39\python.exe
+    C:\Python38\python.exe
+    C:\Python37\python.exe
+    "C:\Program Files\Python312\python.exe"
+    "C:\Program Files\Python311\python.exe"
+    "C:\Program Files\Python310\python.exe"
+    "C:\Program Files\Python39\python.exe"
+    "C:\Program Files\Python38\python.exe"
+    "C:\Program Files\Python37\python.exe"
+    "%LOCALAPPDATA%\Programs\Python\Python312\python.exe"
+    "%LOCALAPPDATA%\Programs\Python\Python311\python.exe"
+    "%LOCALAPPDATA%\Programs\Python\Python310\python.exe"
+    "%LOCALAPPDATA%\Programs\Python\Python39\python.exe"
+    "%LOCALAPPDATA%\Programs\Python\Python38\python.exe"
+    "%LOCALAPPDATA%\Programs\Python\Python37\python.exe"
+) do (
+    if exist %%p (
+        echo Found Python: %%p
+        %%p main.py
+        set FOUND=1
+        pause
+        exit /b
+    )
 )
 
-echo Install Python 3.12...
-winget install --id Python.Python.3.12 --silent --accept-package-agreements --accept-source-agreements
-
-echo Please restart windows.bat
-pause
+if %FOUND% equ 0 (
+    echo Python not found. Installing...
+    
+    winget --version >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Installing Winget...
+        powershell -Command "Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'; Start-Process -FilePath 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -ArgumentList '/quiet' -Wait"
+    )
+    
+    winget install --id Python.Python.3.12 --silent --accept-package-agreements --accept-source-agreements
+    echo Please restart windows.bat
+    pause
+)
