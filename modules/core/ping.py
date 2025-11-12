4c539f47ce84e6f89416689e6ace8fe3db548980
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-11-12 22:29:15 +0300

    Update ping.py

--- modules/core/ping.py
+++ modules/core/ping.py
@@ -9,18 +9,18 @@ Module_Name = 'Ping'
 
 LANGUAGES = {
     "en": {
-        "connect_stable": """<emoji id='5416081784641168838'>🟢</emoji> Stable""",
+        "connect_stable": "<emoji id='5416081784641168838'>🟢</emoji> Stable",
         "connect_good": "🟠 Good",
-        "connect_unstable": """<emoji id='5411225014148014586'>🔴</emoji> Unstable""", 
+        "connect_unstable": "<emoji id='5411225014148014586'>🔴</emoji> Unstable", 
         "connect_bad": "⚠ Check your network connection",
-        "text_return": """<emoji id='5269563867305879894'>🏓</emoji> Pong \n📶 {ping} ms \n {connect}"""
+        "text_return": "<b><emoji id='5269563867305879894'>🏓</emoji> Pong\n<emoji id='5783105032350076195'>📶</emoji></b> {round(ping)} ms\n{connect}"
     },
     "ru": {
-        "connect_stable": """<emoji id='5416081784641168838'>🟢</emoji> Стабильно""",
+        "connect_stable": "<emoji id='5416081784641168838'>🟢</emoji> Стабильно",
         "connect_good": "🟠 Терпимо",
-        "connect_unstable": """<emoji id='5411225014148014586'>🔴</emoji> Нестабильно""",
+        "connect_unstable": "<emoji id='5411225014148014586'>🔴</emoji> Нестабильно",
         "connect_bad": "⚠ Проверьте подключение", 
-        "text_return": """<emoji id='5269563867305879894'>🏓</emoji> Понг \n📶 {ping} мс \n {connect}"""
+        "text_return": "<b><emoji id='5269563867305879894'>🏓</emoji> Понг\n<emoji id='5783105032350076195'>📶</emoji></b> {round(ping)} мс\n{connect}"
     },
 }
 
@@ -49,22 +49,4 @@ async def ping(client, message):
     connect_text = get_text("ping", connect_key, LANGUAGES=LANGUAGES)
     result_text = get_text("ping", "text_return", LANGUAGES=LANGUAGES, ping=round(ping_time), connect=connect_text)
 
-    try:
-        await message.edit(result_text)
-    except Exception as f:
-        #debug
-        print(result_text)
-        await client.send_message(
-            message.chat.id, 
-            result_text, 
-        )
-        await message.edit(f)
-                           
-
-
-
-
-
-
-
-
+    await message.edit(result_text)
